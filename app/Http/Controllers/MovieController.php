a66832acd15f9a48d89b1a82853521e62d6be39f
sorraine <kalifasroberts@gmail.com>
2024-04-07 13:06:26 -0400

    Added get and post routes

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movie;
+use Illuminate\Http\Request;
+
+class MovieController extends Controller
+{
+    public function index()
+    {
+        // Retrieve all movies from the database
+        $movies = Movie::all();
+
+        return response()->json($movies);
+    }
+
+    public function store(Request $request)
+    {
+        // Validate incoming request data
+        $validatedData = $request->validate([
+            'title' => 'required|string',
+            'description' => 'required|string',
+            'poster'=> 'required|file|image',
+
+          
+        ]);
+    
+
+      // Create a new movie record in the database
+         $movieposter = $request->file('poster');
+         $filename = time() . '.' . $movieposter->getClientOriginalExtension(); /**Credits to Fabian Bertrand */
+         $path = $movieposter->storeAs('posters', $filename, 'public');
+    
+            $movie = new Movie();
+            $movie->title = $request->input('title');
+            $movie->description = $request->input('description');
+            $movie->poster = asset('storage/'.$path);
+            $movie->save();
+     
+            return response()->json([
+                'message' => 'Movie created successfully',
+                'movie'  => $movie
+            ], 201);     
+      
+      
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\MovieController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/v1/movies', [MovieController::class,'index']);
+Route::post('/v1/movies', [MovieController::class,'store']);
